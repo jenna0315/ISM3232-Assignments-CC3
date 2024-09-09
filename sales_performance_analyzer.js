1b03529db2108d2ab7118f8698ecbe1b0defaefe
jenna0315 <jbellagamba@usf.edu>
2024-09-08 21:22:41 -0400

    Implement calculateAverageSales function

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -1,1 +1,8 @@
-
+//Implement calculateAverageSales function
+let salesRecordsData = [111,222,333,444]
+function calculateAverageSales(salesRecordsData)
+{let totalSales = salesRecordsData.reduce((sum,sales)=> sum + sales,0)
+let Average = totalSales/salesRecordsData.length
+return Average}
+let AverageSales = calculateAverageSales(salesRecordsData);
+console.log('Average Sales:',AverageSales)
